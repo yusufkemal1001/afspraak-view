bcb638ddcc568074d03f6dd977da4818b8308935
JammingEnd <57032230+JammingEnd@users.noreply.github.com>
2022-05-24 15:20:13 +0100

    oli

--- Non-view Objects/AfspraakClass.php
+++ Non-view Objects/AfspraakClass.php
@@ -1,1 +1,21 @@
 
+<?php 
+
+class Team {
+
+    public $name;
+
+    public $description; 
+
+    public $team;
+
+    public $date;
+
+    public $repeat;
+
+    public int $repeatIndex;
+
+
+}
+
+?>
