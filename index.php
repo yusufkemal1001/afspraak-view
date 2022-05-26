d22abbb35d49bb0cc47d3e97df55de54bbb0378f
yusufkemal1001 <74593967+yusufkemal1001@users.noreply.github.com>
2022-05-26 13:21:15 +0200

    index page styling

--- index.php
+++ index.php
@@ -5,12 +5,44 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="refresh" content="60">
     <link href="/css/style.css" rel="stylesheet">
     <title>Homepage</title>
 </head>
 <body>
- <div class="text-red-600">
-     WASSSUUUUp
- </div>
+    <div class="w-full overflow-x-hidden ">
+        <div class="w-full flex border-b-1 p-5 border-b-4 border-black">
+            <div id="currentTime" class="text-3xl pl-4 max-w-xs">
+                <script>
+                    var today = new Date();
+                    var time = today.getHours() + ":" + today.getMinutes();
+                    document.getElementById("currentTime").innerHTML = time;
+                </script>
+            </div>
+
+            <div class="text-4xl text-center m-auto pr-16">
+                <div>
+                Software Developer Rooster
+                </div>
+            </div>
+        </div>
+        <div class="mainContainer flex m-auto ">
+
+            <div class="w-2/6 border-r-2 border-l-2 border-b-2 border-black">
+                <div class="w-full">
+                    <div class="text-4xl text-center mb-5">Bezig</div>
+
+                    <div class="w-3/4 max-h-80 h-40 bg-red-100 m-auto p-5 mb-5">sup</div>
+                </div>
+            </div>
+            <div class="w-4/6">
+                <div class="w-full border-r-2 border-black border-b-2 ">
+                    <div class="text-4xl text-center mb-5">Week Rooster</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 </body>
 </html>
